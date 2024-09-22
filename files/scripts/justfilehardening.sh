8c732af0e3833c6a8befaff8aa9c08f8d61bc7fb
outbackdingo <outbackdingo@gmail.com>
2024-09-22 14:58:24 +0700

    update for brew

--- files/scripts/justfilehardening.sh
+++ files/scripts/justfilehardening.sh
@@ -3,6 +3,5 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-# patch /usr/share/ublue-os/just/05-brew.just < enable-wheelless-brew-installation.patch 
 patch /usr/share/ublue-os/just/15-luks.just < use-pkexec-in-luks-just.patch
 patch /usr/share/ublue-os/just/40-nvidia.just < remove-unusable-just-command.patch
